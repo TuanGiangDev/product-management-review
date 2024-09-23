a7644d664c038e63ce037763be0359e2a98e4fcf
YangDev <tuangiangdev@gmail.com>
2024-09-23 14:40:59 +0700

    Static - Bootstrap 4
    Nhung bootstrap 4 - Nhung file tinh

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static('public'));
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
