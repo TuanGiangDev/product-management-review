817dce33ba1927cd98f2d9a2fa8cd5fc4f2a4495
YangDev <tuangiangdev@gmail.com>
2024-09-25 21:16:16 +0700

    Tạo trang tổng quan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -16,6 +17,7 @@ app.set("view engine", "pug");
 app.use(express.static('public'));
 
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
