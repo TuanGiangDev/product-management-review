48dbc3b53281656a2e6f93ab5abd61f581b6a8a0
YangDev <tuangiangdev@gmail.com>
2024-09-27 08:59:35 +0700

    Xây dựng layouts chung

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+
 const route = require("./routes/client/index.route");
+
 const routeAdmin = require("./routes/admin/index.route");
+
+const systemPrefix = require("./config/system");
+
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const database = require("./config/database");
@@ -14,8 +20,12 @@ const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+// App Locals Variables
+app.locals.prefixAdmin = systemPrefix.prefixAdmin;
+
 app.use(express.static('public'));
 
+// Routes
 route(app);
 routeAdmin(app);
 
